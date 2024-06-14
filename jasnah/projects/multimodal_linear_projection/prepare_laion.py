13e0ae386264f5d3ec3427bf29eb9ea2d496755f
Alexander Comerford <alex@taoa.io>
2024-06-14 14:48:55 +0000

    add prepare laion

---
+++ jasnah/projects/multimodal_linear_projection/prepare_laion.py
@@ -0,0 +1,120 @@
+import concurrent.futures
+import io
+import json
+import multiprocessing as mp
+import warnings
+from collections import defaultdict
+from itertools import islice, product
+from pathlib import Path
+from typing import Iterator, List, Tuple
+
+import httpx
+from PIL import Image
+from tqdm import tqdm
+
+import datasets
+import jasnah
+import jasnah.dataset
+
+warnings.filterwarnings("ignore")
+
+MAX_CONCURRENT = 128
+NUM_PROCESS = 128
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"
+}
+
+
+def fetch_image(datum):
+    try:
+        with httpx.Client() as client:
+            response = client.get(datum["URL"], timeout=3, headers=headers, follow_redirects=True)
+            response.raise_for_status()
+            image = Image.open(io.BytesIO(response.content)).convert("RGB")
+            ## sanity checks on size
+            assert image.height > 10
+            assert image.width > 10
+            return (image, datum)
+    except Exception as e:
+        return None
+
+
+def generate_descriptions(input: datasets.Dataset, samples: int, pos: int):
+    input: datasets.Dataset = input[0]
+    samples: int = samples[0]
+    samples = samples[1] - samples[0]
+    pos: int = pos[0]
+
+    remaining = samples
+    ds = input.select(range(pos, len(input), NUM_PROCESS))
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        tasks = iter(executor.submit(fetch_image, datum=datum) for datum in ds)
+        futures = list(islice(tasks, MAX_CONCURRENT))
+        while futures:
+            completed, ongoing_futures = concurrent.futures.wait(
+                futures, return_when=concurrent.futures.FIRST_COMPLETED
+            )
+            futures = list(ongoing_futures)
+            for completed_future in completed:
+
+                result = completed_future.result()
+                if result is not None:
+                    remaining -= 1
+                    image, datum = result
+                    yield {
+                        "image": image,
+                        "description": datum["TEXT"],
+                    }
+
+                try:
+                    if remaining > 0:
+                        next_task = next(tasks)
+                        futures.append(next_task)
+                    else:
+                        return
+                except StopIteration:
+                    continue
+
+
+def chunkify(n, num_chunks):
+    """Split n into num_chunks chunks. Any remainder is split evenly among the chunks."""
+    chunk_size = n // num_chunks
+    remainder = n % num_chunks
+    start = 0
+    for i in range(num_chunks):
+        end = start + chunk_size + (1 if i < remainder else 0)
+        yield start, end
+        start = end
+
+
+def main():
+    SUFFIX = ""
+
+    n = 1_000_000
+    path = jasnah.dataset.get_dataset("laion400m_metadata")
+    ds = jasnah.dataset.load_dataset("laion400m_metadata")
+
+    processed = path / "processed"
+    processed.mkdir(exist_ok=True, parents=True)
+
+    ds_description = datasets.Dataset.from_generator(
+        generate_descriptions,
+        gen_kwargs={
+            "input": [ds for _ in range(NUM_PROCESS)],
+            "samples": list(chunkify(n, NUM_PROCESS)),
+            "pos": list(range(NUM_PROCESS)),
+        },
+        num_proc=NUM_PROCESS,
+        features=datasets.Features(
+            {
+                "image": datasets.Image(),
+                "description": datasets.Value("string"),
+            }
+        ),
+    )
+    ds_description.save_to_disk(str(processed / f"descriptions{SUFFIX}"))
+
+
+if __name__ == "__main__":
+    main()

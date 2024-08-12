6fea65bd11cb76744adda2bddb2b662a92cf7be2
Vadim <support@zavodil.ru>
2024-08-12 17:27:05 +0300

    fix: `nonce` creation query to compatible with singlestore

--- hub/migrations/20240723113330_nonce.sql
+++ hub/migrations/20240723113330_nonce.sql
@@ -5,19 +5,12 @@ USE hub;
 -- account_id is a near account, max 64 characters
 CREATE TABLE
     nonces (
-        nonce VARCHAR(32),
-        account_id VARCHAR(64),
-        message TEXT NOT NULL,
-        recipient VARCHAR(64) NOT NULL,
-        callback_url TEXT NULL,
-        nonce_status ENUM ('active', 'revoked') NOT NULL,
-        first_seen_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        PRIMARY KEY (account_id, nonce, message (255), recipient),
-        UNIQUE KEY unique_nonce (
-            account_id,
-            nonce,
-            message (255),
-            callback_url (255),
-            recipient
-        )
-    );
+       nonce VARCHAR(32),
+       account_id VARCHAR(64),
+       message TEXT NOT NULL,
+       recipient VARCHAR(64) NOT NULL,
+       callback_url TEXT NULL,
+       nonce_status ENUM ('active', 'revoked') NOT NULL,
+       first_seen_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+       PRIMARY KEY (account_id, nonce, message, recipient, callback_url)
+);

59a711b7b0ff97e49fabd5b97b8b74bdd560d408
Marcelo Fornet <mfornet94@gmail.com>
2024-08-15 13:49:33 +0200

    fix: pretty print version of near-opanapi.json

--- scripts/generate_client.sh
+++ scripts/generate_client.sh
@@ -2,7 +2,7 @@
 
 # manually update near-openapi.json
 # it can be fetched from the locally running hub at http://localhost:9000/openapi.json
-# curl http://localhost:9000/openapi.json > near-openapi.json
+# curl http://localhost:9000/openapi.json | jq  > near-openapi.json
 
 # https://openapi-generator.tech/docs/generators/python
 openapi-generator-cli generate -i near-openapi.json -g python -o /tmp/nearai_api_client/

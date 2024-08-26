b7e3af7518e4ac247356355dca87bce8f5c94084
Pierre LE GUEN <26087574+PierreLeGuen@users.noreply.github.com>
2024-08-26 14:03:06 +0200

    Docs inference (#201)
    * Added docs for inference.
    
    * Added inference.md to mkdocs conf.
    
    * Fixing CI

---
+++ hub/examples/chat_openai_client.py
@@ -0,0 +1,24 @@
+import openai
+import json
+import os
+import nearai
+
+hub_url = "https://api.near.ai/v1"
+
+# Login to NEAR AI Hub using nearai CLI.
+# Read the auth object from ~/.nearai/config.json
+auth = nearai.config.load_config_file()["auth"]
+signature = json.dumps(auth)
+
+client = openai.OpenAI(base_url=hub_url, api_key=signature)
+
+# list models available from NEAR AI Hub
+models = client.models.list()
+print(models)
+
+# create a chat completion
+chat_completion = client.chat.completions.create(
+    model="fireworks::accounts/fireworks/models/llama-v3-8b-instruct-hf",
+    messages=[{"role": "user", "content": "Hello, world!"}],
+)
+print(chat_completion)

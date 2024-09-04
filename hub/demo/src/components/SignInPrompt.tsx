3d6c47f5f0c4e1050b8b19b91e53eaef745ff82e
zavodil <support@zavodil.ru>
2024-09-04 15:00:22 -0400

    Fix agent header (#259)
    * feat: display agent header if it was specified in
    {"details":
    	"agent": {
    		"title": ...,
    		"icon": ..., //web only
    		"welcome_message": ...
    	}
    }
    
    Check how it works on `zavodil.near/what-beats-rock/0.15/run`
    
    * feat: metadata structure updated
    fix: css inverted for dark mode
    
    * fix: icon css
    
    * fix: linter

--- hub/demo/src/components/SignInPrompt.tsx
+++ hub/demo/src/components/SignInPrompt.tsx
@@ -11,7 +11,7 @@ import { Section } from './lib/Section';
 import { Text } from './lib/Text';
 
 interface SignInPromptProps {
-  props: {
+  props?: {
     showWelcome?: boolean;
   };
 }
@@ -20,7 +20,7 @@ export const SignInPrompt = ({ props }: SignInPromptProps) => {
   return (
     <Container size="s" style={{ margin: 'auto', textAlign: 'center' }}>
       <Flex direction="column" gap="m" align="center">
-        {props.showWelcome && <Text size="text-l">Welcome</Text>}
+        {props?.showWelcome && <Text size="text-l">Welcome</Text>}
         <Text>Please sign in with your NEAR wallet to continue</Text>
         <Button
           variant="affirmative"

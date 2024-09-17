1054671068aafa629b198b66166cec75b6a606b2
Anton Lomonos <arktoswb@gmail.com>
2024-09-17 16:27:03 -0400

    debugging

--- hub/api/v1/registry.py
+++ hub/api/v1/registry.py
@@ -442,7 +442,7 @@ async def list_entries(
 
         entries_info.sort(key=lambda x: x.id, reverse=True)
         # DEBUGGING
-        entries_info = []
+        # entries_info = []
 
         if category == "model" and len(entries_info) < total and namespace == "" and tags == "" and starred_by == "":
             provider_models_list = provider_models.get_unregistered_common_provider_models(total - len(entries_info))

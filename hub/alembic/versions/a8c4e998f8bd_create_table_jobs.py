ff6b5e4077020abff4e86b3f2cd06b2550f42d39
Marcelo Fornet <mfornet94@gmail.com>
2024-11-01 14:58:02 +0000

    fix alembic linked list

--- hub/alembic/versions/a8c4e998f8bd_create_table_jobs.py
+++ hub/alembic/versions/a8c4e998f8bd_create_table_jobs.py
@@ -1,7 +1,7 @@
 """Create table jobs.
 
 Revision ID: a8c4e998f8bd
-Revises: 212e5ca48274
+Revises: 71e2bccbe470
 Create Date: 2024-10-23 11:32:24.054255
 
 """
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "a8c4e998f8bd"
-down_revision: Union[str, None] = "212e5ca48274"
+down_revision: Union[str, None] = "71e2bccbe470"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 

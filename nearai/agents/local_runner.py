10d74e9eb709f9a4b3cc40f595022bebbaea1db1
arktoswb <arktoswb@users.noreply.github.com>
2024-12-09 07:16:35 -0800

    Reduce verbosity of agent runs (#508)

--- nearai/agents/local_runner.py
+++ nearai/agents/local_runner.py
@@ -1,11 +1,5 @@
-import logging
-
 from nearai.aws_runner.service import run_with_environment
 from nearai.shared.auth_data import AuthData
-
-DEFAULT_OUTPUT_PATH = "/tmp/nearai/conversations/"
-
-logger = logging.getLogger(__name__)
 
 
 class LocalRunner:
@@ -18,11 +12,4 @@ class LocalRunner:
         auth: AuthData,
         params: dict,
     ) -> None:
-        print(
-            f"Initializing LocalRunner with path: {path}, agents: {agents}, thread_id: {thread_id},"
-            f"run_id: {run_id}, auth: {auth}, params: {params}"
-        )
-
-        self._agents = agents
-        self._confirm_commands = True
         run_with_environment(agents, auth, thread_id, run_id, additional_path=path, params=params)

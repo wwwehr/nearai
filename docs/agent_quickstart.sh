3fc906f0c6d4e115a64f0bc5ad986dbe102462fe
arktoswb <arktoswb@users.noreply.github.com>
2024-12-09 17:18:12 -0800

    Remove 'open' lines from quickstart as they may cause unexpected behavior (#628)

--- docs/agent_quickstart.sh
+++ docs/agent_quickstart.sh
@@ -3,8 +3,8 @@
 mkdir -p ~/.nearai/registry/user/example_agent/0.0.1
 nearai registry metadata_template ~/.nearai/registry/user/example_agent/0.0.1 agent --description="Where would you like to travel?"
 cat docs/examples/example_agent.py > ~/.nearai/registry/user/example_agent/0.0.1/agent.py
-open ~/.nearai/registry/user/example_agent/0.0.1/metadata.json
-open ~/.nearai/registry/user/example_agent/0.0.1/agent.py
+echo "Consider editing ~/.nearai/registry/user/example_agent/0.0.1/metadata.json"
+echo "Consider editing ~/.nearai/registry/user/example_agent/0.0.1/agent.py"
 echo "Starting example_agent in interactive mode..."
 echo "\t type 'exit' to quit."
 nearai agent interactive ~/.nearai/registry/user/example_agent/0.0.1 --local

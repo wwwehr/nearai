6fb42d2e73f0b72566f606ac588becdbae207981
Gabe Hamilton <gabe@near.org>
2025-05-01 11:44:19 -0600

    Set the X-Accel-Buffering header to no to allow SSE events to work with nginx (#1133)

--- hub/api/v1/thread_routes.py
+++ hub/api/v1/thread_routes.py
@@ -960,7 +960,11 @@ async def thread_subscribe(thread_id: str, run_id: Optional[str] = None, auth: A
             raise HTTPException(status_code=404, detail="Run for thread not found")
         _check_thread_permissions(auth, session, thread_id)
 
-        return StreamingResponse(stream_run_events(run.id, False), media_type="text/event-stream")
+        return StreamingResponse(
+            stream_run_events(run.id, False),
+            media_type="text/event-stream",
+            headers={"X-Accel-Buffering": "no", "Content-Type": "text/event-stream", "Cache-Control": "no-cache"},
+        )
 
 
 @threads_router.get("/threads/{thread_id}/runs/{run_id}")
